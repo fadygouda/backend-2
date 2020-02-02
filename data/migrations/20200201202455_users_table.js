cfa3513be2cd21f0aea02499c95c3a559f18cffa
Code4Blessings <robinwarden@code4blessings.com>
2020-02-02 16:53:32 -0500

    Updated songs-router and added 2 more tables to migrations

--- data/migrations/20200201202455_users_table.js
+++ data/migrations/20200201202455_users_table.js
@@ -5,8 +5,21 @@ exports.up = function(knex) {
       tbl.string('username', 128).notNullable().unique();
       tbl.string('password', 255).notNullable();
   })
+  .createTable('songs', tbl => {
+    tbl.increments();
+    tbl.integer('user_id').notNullable().references('id').inTable('users')
+    tbl.string('song_title', 255).notNullable()
+    tbl.string('artist').notNullable()
+    tbl.boolean('favorite').defaultTo(false).notNullable()
+  })
+  .createTable('users_songs', tbl => {
+    tbl.increments()
+    tbl.integer('user_id').references('id').inTable('users')
+    tbl.integer('song_id').references('id').inTable('songs')
+  })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists('users')
+    .dropTableIfExists('songs')
 };
--- users/user-router.js
+++ users/user-router.js
@@ -4,4 +4,6 @@ const db = require("../data/dbConfig");
 
 const Users = require("../users/user-model");
 
+
+
 module.exports = router;
